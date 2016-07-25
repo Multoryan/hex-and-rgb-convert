048806c6499f6a5707b21050a8a7aba0529a37e8
RandomIt <79134682762@ya.ru>
2016-07-25 18:43:07 +0600

    Версия 2
    + Одно поле для ввода для HEX и RGB
    + Нет кнопок, событие по нажатию клавиш в поле
    + Маски для HEX: с # или без нее, 3 символа или 6
    + Маски для RGB: x,x,x или rgb(x,x,x)
    + Пробелы ни на что не влияют
    
    - Не работает вставка с помощью мыши
    - Нет оформления
    - Не ясно как вводить данные

--- js/script.js
+++ js/script.js
@@ -1,19 +1,72 @@
 "use strict";
 
-var btnSend = document.getElementById("convert");
+/*var btnTestHEX = document.getElementById("inputHEX");
+var btnTestRGB = document.getElementById("inputRGB");
+btnTestHEX.addEventListener("keyup", convertToRGB);
+btnTestRGB.addEventListener("keyup", convertToHEX);*/
 
-btnSend.addEventListener("click", convert);
+var btnTest = document.getElementById("inputColor");
+btnTest.addEventListener("keyup", convert);
+
 
 function convert(){
-	var hex = document.getElementById("inputHEX").value;
+	var color = document.getElementById("inputColor").value;
+	color = color.replace(/\s+/g, ''); //Удаление пробелов
+	var patternHEX = /^#?([0-9a-f]{3}){1,2}$/i;
+	var patternRGB = /(^rgb\()?(\d{1,3}),(\d{1,3}),(\d{1,3})(\))?/;
+	if(patternHEX.test(color) == false && patternRGB.test(color) == false){
+		document.getElementById("result").innerHTML = "";
+	}
+	if(patternHEX.test(color)){
+		convertToRGB(color);
+	}
+	if(patternRGB.test(color)){
+		convertToHEX(color);
+	}
+}
+
+
+function convertToRGB(hex){
+
+	if(hex[0] == "#"){
+		hex = hex.substr(1);
+	}
+	
 	var hexToDiad = [];
-	hexToDiad[0] = hex.substr(0,2);
-	hexToDiad[1] = hex.substr(2,2);
-	hexToDiad[2] = hex.substr(4,2);
+	
+	if(hex.length == 3){
+		hexToDiad[0] = hex.substr(0,1) + hex.substr(0,1);
+		hexToDiad[1] = hex.substr(1,1) + hex.substr(1,1);
+		hexToDiad[2] = hex.substr(2,1) + hex.substr(2,1);
+	}else{
+		hexToDiad[0] = hex.substr(0,2);
+		hexToDiad[1] = hex.substr(2,2);
+		hexToDiad[2] = hex.substr(4,2);
+	}
+	
 	var temp;
 	hexToDiad.forEach(function(item, i, arr) {
 		temp = "0x" + item;
 		arr[i] = parseInt(temp).toString(10);
 	});
-	document.getElementById("inputRGB").value = hexToDiad.join();
+	document.getElementById("result").innerHTML = "rgb ( " + hexToDiad.join() + " )";
+}
+
+function convertToHEX(rgb){
+	
+	if(rgb[0] == "r"){
+		rgb = rgb.substr(4);
+		rgb = rgb.slice(0,-1);
+	}
+	console.log(rgb);
+	
+	var rgbToTriad = rgb.split(',');
+	var temp;
+	rgbToTriad.forEach(function(item, i, arr) {
+		temp = +item;
+		arr[i] = parseInt(temp).toString(16);
+		if(arr[i].length == 1)
+			arr[i] = "0" + arr[i];
+	});
+	document.getElementById("result").innerHTML = "#"+rgbToTriad.join('');
 }
